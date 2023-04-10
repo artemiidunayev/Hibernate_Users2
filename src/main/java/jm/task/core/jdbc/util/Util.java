dcc21e6b093f270d6f215563010c553bf9839884
artemiidunayev <artemdunaev606@gmail.com>
2023-04-10 21:46:34 +0300

    "настроил соединение с БД в классе Util"

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+    private static final String url = "jdbc:mysql:// localhost:3306/mydbtest";
+    private static final String user = "root";
+    private static final String password = "edifif77artem";
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
     // реализуйте настройку соеденения с БД
 }
